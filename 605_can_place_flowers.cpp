e45be00f5ff6b118e850a27894510b5e25eff0aa
rian <rianrbps@gmail.com>
2025-02-15 22:51:18 -0800

    array/string

--- 605_can_place_flowers.cpp
+++ 605_can_place_flowers.cpp
@@ -3,7 +3,7 @@ class Solution {
         bool canPlaceFlowers(vector<int>& flowerbed, int n) {
             int count = 0;
             for(int i = 0; i <flowerbed.size(); i++){
-                if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i ==flowerbed.size() - 1 || flowerbed[i+1] == 0)) {
+                if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i+1] == 0)) {
                     flowerbed[i] = 1;
                     count++;
                 }
